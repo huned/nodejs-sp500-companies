6964b8686d5ae681750ceb3bd034b34bc55e4184
Huned Botee <huned@hunedbotee.com>
2021-10-31 15:12:21 -0700

    Get everything kinda working.

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
 #!/usr/bin/env node
 
-import 'dotenv/config'
-import debugModule from 'debug'
-const debug = debugModule('index.js')
-
-// NOTE: YUCK. See https://github.com/nodejs/modules/issues/274
-const isCalledDirectly = () => import.meta.url === `file://${process.argv[1]}`
-
-if (isCalledDirectly()) {
-  debug('called directly')
-} else {
-  debug('required as a module')
-}
-
-// TODO: Put your code here
-process.stdout.write('put your code in ./index.js\n')
-
-export default () => {
-  // TODO: Also put your code here
-}
+export { default } from './lib/sp500-constituents-table.js'
--- test/test_index.js
+++ test/test_index.js
@@ -1,10 +1,24 @@
 import assert from 'assert'
-import starter from '../index.js'
+import nock from 'nock'
+import * as fs from 'fs/promises'
+import sp500Constituents from '../index.js'
 
 // NOTE: don't use arrow functions with mocha.
 // See https://github.com/mochajs/mocha/issues/2018
-describe('Index', function () {
+describe('S&P500 Constituents', function () {
   it('does not raise', function () {
-    assert.doesNotThrow(starter)
+    assert.doesNotThrow(sp500Constituents)
+  })
+
+  it('returns companies and metadata', async function () {
+    const mockHTML = await fs.readFile('./test/data/List of S&P 500 companies - Wikipedia.html')
+    nock('http://en.wikipedia.org')
+      .get('/wiki/List_of_S%26P_500_companies')
+      .reply(200, mockHTML)
+    const mockFile = './data/List of S&P 500 companies - Wikipedia.html'
+    const table = await sp500Constituents(mockFile)
+
+    // 1 header row + 505 company rows = 506 rows
+    assert.strictEqual(table.length, 506)
   })
 })
