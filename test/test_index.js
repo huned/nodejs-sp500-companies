e11b477f145ee55b1e024eed0e3821e65cfd65b6
Huned Botee <huned@hunedbotee.com>
2021-11-01 10:49:53 -0700

    Properly parse companies table and return an array of JSON objects.

--- test/test_index.js
+++ test/test_index.js
@@ -6,19 +6,41 @@ import sp500Constituents from '../index.js'
 // NOTE: don't use arrow functions with mocha.
 // See https://github.com/mochajs/mocha/issues/2018
 describe('S&P500 Constituents', function () {
-  it('does not raise', function () {
-    assert.doesNotThrow(sp500Constituents)
-  })
+  let results = null
 
-  it('returns companies and metadata', async function () {
+  beforeEach(async function () {
     const mockHTML = await fs.readFile('./test/data/List of S&P 500 companies - Wikipedia.html')
     nock('http://en.wikipedia.org')
       .get('/wiki/List_of_S%26P_500_companies')
       .reply(200, mockHTML)
-    const mockFile = './data/List of S&P 500 companies - Wikipedia.html'
-    const table = await sp500Constituents(mockFile)
+    results = await sp500Constituents()
+  })
 
-    // 1 header row + 505 company rows = 506 rows
-    assert.strictEqual(table.length, 506)
+  it('returns correct length array', function () {
+    assert.strictEqual(results.length, 505)
+  })
+
+  it('returns correct JSON object structure', function () {
+    const result = results[0]
+    assert.deepStrictEqual(result, {
+      ticker: 'MMM',
+      name: '3M',
+      exchange: 'NYSE',
+      cik: '0000066740',
+      gics: {
+        sector: 'Industrials',
+        subIndustry: 'Industrial Conglomerates'
+      },
+      dateAdded: '1976-08-09',
+      yearFounded: '1902'
+    })
+  })
+
+  it('all companies have a known exchange', function () {
+    for (let i = 0; i < results.length; i++) {
+      const result = results[i]
+      // console.log(result)
+      assert.notEqual(result.exchange, 'UNKNOWN')
+    }
   })
 })
